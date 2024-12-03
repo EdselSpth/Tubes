669234f76c3417807d84c2ee0e9d45eab90ef929
EdselSpth <edselspth@gmail.com>
2024-12-03 08:09:04 +0700

    make addVertex  on tubes.cpp

--- tubes.cpp
+++ tubes.cpp
@@ -1,3 +1,10 @@
 #include "tubes.h"
 #include <iostream>
 using namespace std;
+
+void createVertex(string namaGedung, adrVertex &V){
+    V = new vertex;
+    namaGedung(V) = namaGedung;
+    nextVertex(V) = NULL;
+    firstEdge(V) = NULL;
+}
